4d5fe40f427301c514d2f8bc75f8ee67ede3366f
amirizad <info@amirizad.com>
2017-03-24 11:48:45 -0700

    Minor Corrections in modulr 7

--- module7/logic.js
+++ module7/logic.js
@@ -3,7 +3,7 @@ $("#factButton").on("click", function() {
 	$("#factText").text(booFacts[number]);
 })
 
-var booFacts = ["Boo is a pomeranian, Boo's best friend is another pomeranian named Buddy, Boo the Pomeranian was born on March 16, making him a Pisces, Boo's favourite food is grass, Boo has released two books"];
+var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16", "making him a Pisces", "Boo's favourite food is grass", "Boo has released two books"];
 
 $("#textPink").on("click", function() {
 	$("#funText").css("color", "pink");
